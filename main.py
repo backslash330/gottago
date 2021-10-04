89619ddd34552ec67168dab6ec92818f9bbef9da
backslash330 <n.almeida330@gmail.com>
2021-10-03 23:15:28 -0600

    Started work on main() function.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-# test of git connection
-print("Hello World")
-print("Linux works too!")
+from selenium import webdriver
+
+def main():
+    path = "C:\Program Files (x86)\chromedriver.exe"
+    driver = webdriver.Chrome(path)
+    driver.get("https://store.brandonsanderson.com/collections/signed-leatherbound-books/products/elantris-leather-bound-book?variant=32867344056400")
+
+if __name__ == '__main__':
+    main()
